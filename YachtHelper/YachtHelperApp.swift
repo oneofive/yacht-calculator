78cdbbf2828ed1a07c298249d56ae6a19262af52
oneofive <10thouseo@naver.com>
2026-01-19 13:28:59 +0900

    Initial Commit

---
+++ YachtHelper/YachtHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  YachtHelperApp.swift
+//  YachtHelper
+//
+//  Created by Manseo Han on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct YachtHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
